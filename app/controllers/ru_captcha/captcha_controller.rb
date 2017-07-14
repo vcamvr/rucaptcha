0366348a47403ff69a1a7aa5088dcd419a156ff0
Xu Yunlong <bjutales@hotmail.com>
2017-07-14 17:24:22 +0800

    Update key

--- app/controllers/ru_captcha/captcha_controller.rb
+++ app/controllers/ru_captcha/captcha_controller.rb
@@ -5,7 +5,7 @@ module RuCaptcha
       headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
       headers['Pragma'] = 'no-cache'
       captcha_key = SecureRandom.uuid
-      headers['X-CAPTCHA-KEY'] = captcha_key
+      headers['X-RUCAPTCHA-KEY'] = captcha_key
       cookies[:captcha_key] = captcha_key
       data = generate_rucaptcha
       opts = { disposition: 'inline', type: 'image/gif' }
--- lib/rucaptcha/version.rb
+++ lib/rucaptcha/version.rb
@@ -1,3 +1,3 @@
 module RuCaptcha
-  VERSION = '2.1.3.1'
+  VERSION = '2.1.3.2'
 end
